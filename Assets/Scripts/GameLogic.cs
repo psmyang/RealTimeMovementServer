3d3a08c272cae847a0bf0b45311a06e8d466e361
psmyang <ymk1234567890ymk@sina.com>
2021-12-15 15:25:22 -0500

    Initial commit

---
+++ Assets/Scripts/GameLogic.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameLogic : MonoBehaviour
+{
+    void Start()
+    {
+        NetworkedServerProcessing.SetGameLogic(this);
+    }
+
+    void Update()
+    {
+
+    }
+
+}
---
+++ Assets/Scripts/NetworkedServerProcessing.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+static public class NetworkedServerProcessing
+{
+
+    #region Send and Receive Data Functions
+    static public void ReceivedMessageFromClient(string msg, int clientConnectionID)
+    {
+        Debug.Log("msg received = " + msg + ".  connection id = " + clientConnectionID);
+
+        string[] csv = msg.Split(',');
+        int signifier = int.Parse(csv[0]);
+
+        if (signifier == ClientToServerSignifiers.asd)
+        {
+
+        }
+        // else if (signifier == ClientToServerSignifiers.asd)
+        // {
+
+        // }
+
+        //gameLogic.DoSomething();
+    }
+    static public void SendMessageToClient(string msg, int clientConnectionID)
+    {
+        networkedServer.SendMessageToClient(msg, clientConnectionID);
+    }
+
+    static public void SendMessageToClientWithSimulatedLatency(string msg, int clientConnectionID)
+    {
+        networkedServer.SendMessageToClientWithSimulatedLatency(msg, clientConnectionID);
+    }
+
+    
+    #endregion
+
+    #region Connection Events
+
+    static public void ConnectionEvent(int clientConnectionID)
+    {
+        Debug.Log("New Connection, ID == " + clientConnectionID);
+    }
+    static public void DisconnectionEvent(int clientConnectionID)
+    {
+        Debug.Log("New Disconnection, ID == " + clientConnectionID);
+    }
+
+    #endregion
+
+    #region Setup
+    static NetworkedServer networkedServer;
+    static GameLogic gameLogic;
+
+    static public void SetNetworkedServer(NetworkedServer NetworkedServer)
+    {
+        networkedServer = NetworkedServer;
+    }
+    static public NetworkedServer GetNetworkedServer()
+    {
+        return networkedServer;
+    }
+    static public void SetGameLogic(GameLogic GameLogic)
+    {
+        gameLogic = GameLogic;
+    }
+
+    #endregion
+}
+
+#region Protocol Signifiers
+static public class ClientToServerSignifiers
+{
+    public const int asd = 1;
+}
+
+static public class ServerToClientSignifiers
+{
+    public const int asd = 1;
+}
+
+#endregion
